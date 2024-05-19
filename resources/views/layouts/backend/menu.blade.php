8484701665cfd26872f9d634392c0814dc2d04f6
firchun025 <firchun025@gmail.com>
2024-05-19 21:32:35 +0900

    fix icon

--- resources/views/layouts/backend/menu.blade.php
+++ resources/views/layouts/backend/menu.blade.php
@@ -6,7 +6,8 @@
         <ul class="navbar-nav theme-brand flex-row  text-center">
             <li class="nav-item theme-logo">
                 <a href="{{ url('/') }}">
-                    <img src="{{ asset('backend_theme') }}/assets/img/90x90.jpg" class="navbar-logo" alt="logo">
+                    <img src="{{ asset('backend_theme') }}/assets/img/favicon.ico" class="navbar-logo" alt="logo"
+                        style="width: 39px; height:39px; object-fit:cover;">
                 </a>
             </li>
             <li class="nav-item theme-text">
@@ -34,12 +35,7 @@
             <li class="menu {{ request()->is('home') ? 'active' : '' }}">
                 <a href="{{ url('/home') }}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="feather feather-home">
-                            <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
-                            <polyline points="9 22 9 12 15 12 15 22"></polyline>
-                        </svg>
+                        <i class="bx bx-home bx-sm" style="vertical-align: middle;"></i>
                         <span>Dashboard</span>
                     </div>
                 </a>
@@ -54,11 +50,7 @@
             <li class="menu {{ request()->is('customers') ? 'active' : '' }}">
                 <a href="{{ url('/customers') }}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="feather feather-message-square">
-                            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                        </svg>
+                        <i class="bx bx-user bx-sm" style="vertical-align: middle;"></i>
                         <span>Customers</span>
                     </div>
                 </a>
@@ -73,11 +65,7 @@
             <li class="menu {{ request()->is('users') ? 'active' : '' }}">
                 <a href="{{ url('/users') }}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="feather feather-message-square">
-                            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                        </svg>
+                        <i class="bx bx-user bx-sm" style="vertical-align: middle;"></i>
                         <span>Users</span>
                     </div>
                 </a>
@@ -92,11 +80,7 @@
             <li class="menu {{ request()->is('profile') ? 'active' : '' }}">
                 <a href="{{ route('profile') }}" aria-expanded="false" class="dropdown-toggle">
                     <div class="">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                            stroke-linejoin="round" class="feather feather-message-square">
-                            <path d="M21 15a2 2 0 0 1-2 2H7l-4 4V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2z"></path>
-                        </svg>
+                        <i class="bx bx-user bx-sm" style="vertical-align: middle;"></i>
                         <span>Profile</span>
                     </div>
                 </a>
